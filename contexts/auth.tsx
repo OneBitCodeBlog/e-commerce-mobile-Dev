e9dc4f90312fcb91db950860823aa0d93ed473f7
André Rodrigues <andre.goncalves.rodrigues@gmail.com>
2021-11-29 15:15:48 -0300

    Adding validate token feat

--- contexts/auth.tsx
+++ contexts/auth.tsx
@@ -1,21 +1,43 @@
-import React, { useState, createContext, useContext } from 'react';
+import React, { useState, createContext, useContext, useEffect } from 'react';
 import User from '../DTOs/user';
+import UserService from '../services/user';
 
 interface AuthProps {
   loggedUser?: User;
   setLoggedUser?: any;
+  isLoading?: boolean;
 };
 
 const AuthContext = createContext<AuthProps>({} as AuthProps);
 
 const AuthProvider: React.FC = ({ children }) => {
   const [loggedUser, setLoggedUser] = useState<User>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    const validate = async () => {
+      try {
+        const resp = await UserService.validateToken();
+
+        const { id, email, name } = resp?.data;
+        setLoggedUser({ id, email, name });
+      } catch(error) {
+        console.log(error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    validate();
+  }, [])
 
   return (
     <AuthContext.Provider
       value={{
         loggedUser,
-        setLoggedUser
+        setLoggedUser,
+        isLoading
       }}
     >
       {children}
--- services/user.ts
+++ services/user.ts
@@ -10,12 +10,20 @@ interface SignInResponse {
   data: User;
 }
 
+interface ValidateTokenResponse {
+  data: User;
+}
+
 const UserService = {
   signIn: ({ email, password }: SignInData) => {
     return api.post<SignInResponse>('auth/v1/user/sign_in', {
       email, 
       password
     });
+  },
+  validateToken: () => {
+    return api.get<ValidateTokenResponse>('auth/v1/user/validate_token')
+      .then(resp => resp?.data);
   }
 }
 
